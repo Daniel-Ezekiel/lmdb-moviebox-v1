c74ec7700befaa7903b4e3491e2f4d3569532941
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-11-30 13:25:55 +0100

    chore: set up search page

--- src/components/global/SearchBox.tsx
+++ src/components/global/SearchBox.tsx
@@ -7,19 +7,21 @@ const SearchBox = ({ activePage }: { activePage: string }) => {
 
   return (
     <form
-      action=''
+      action={`/search/${searchQuery}`}
+      method='GET'
       className='mb-2 row-start-2 col-span-full md:col-start-2 md:row-start-1 md:col-span-1 md:mb-[0] xl:ml-8'
     >
       <div className='relative min-w-full'>
         <input
           type='text'
           value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          onChange={(e) => setSearchQuery(e.target.value.trim())}
           placeholder='Search for a movie, tv show, or person'
+          required
           className={`min-w-full p-2 rounded-lg bg-[transparent] border-[0.1rem] border-gray-200 placeholder:text-gray-200 text-white text-base`}
         />
         <button
-          type='button'
+          // type='button'
           className='absolute top-1/2 right-2 -translate-y-1/2'
         >
           {" "}
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import MainLayout from "../layout/MainLayout";
 
 const Search = () => {
+  const [category, setCategory] = useState("movies");
+
   return (
     <MainLayout showHeader={true} activePage='search' showFooter={true}>
-      <h1>Search</h1>
+      <section className='max-w-[124rem] mx-auto grid grid-cols-[1fr,_auto] gap-3 p-3 md:py-6'>
+        <div className='flex items-center'>
+          <h1 className='font-bold text-3xl xl:text-5xl'>Search Results</h1>
+        </div>
+
+        <div className='justify-self-end align-self-center'>
+          <select
+            name='category'
+            id='category'
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className='h-full  rounded-lg p-2 border-[0.1rem] shadow-lg text-base xsm:w-[15rem]'
+          >
+            <option value='movie'>Movies</option>
+            <option value='tv'>TV Shows</option>
+            <option value='person'>People</option>
+            {/* <option value=""></option> */}
+          </select>
+        </div>
+
+        <div className='col-span-full'>Real search results on page load...</div>
+      </section>
     </MainLayout>
   );
 };
